1bb8e400b05a33584cd1c6f05444af946748b342
Ebrahim Karam <ebrahim_karam@hotmail.com>
2021-03-04 15:53:21 -0500

    Added a page for the HAP GUI
    The HAP GUI should help out

--- zoom_sdk_demo/App.xaml.cs
+++ zoom_sdk_demo/App.xaml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-using ZOOM_SDK_DOTNET_WRAP;
+﻿using System.Windows;
 
 namespace zoom_sdk_demo
 {
