f677625b8886ae8d90ac9a217cbd969f8bbdbdcc
Ebrahim Karam <ebrahim_karam@hotmail.com>
2021-03-11 14:02:42 -0500

    Added the BO Setting XAML template
    Have a template of what we would like

---
+++ zoom_sdk_demo/BO_Settings_Window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace zoom_sdk_demo
+{
+    /// <summary>
+    /// Interaction logic for BO_Settings_Window.xaml
+    /// </summary>
+    public partial class BO_Settings_Window : Window
+    {
+        public BO_Settings_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
