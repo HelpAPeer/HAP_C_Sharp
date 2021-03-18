9f2ef0199ac76036763246e7d7ae80ee03a36ca2
Ebrahim Karam <ebrahim_karam@hotmail.com>
2021-03-18 12:36:01 -0400

    Merge remote-tracking branch 'origin/master' into reckert-fix

---
+++ zoom_sdk_demo/HAP_MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using zoom_sdk_demo.Models;
+
+namespace zoom_sdk_demo
+{
+    /// <summary>
+    /// Interaction logic for HAP_MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class HAP_MainWindow : Window
+    {
+        public ObservableCollection<Question> questions;
+        public int id_lastSelected = 0;
+
+        public HAP_MainWindow()
+        {
+            this.DataContext = this;
+
+            // check if participants in meeting is empty. If it is, let's fill with dummy data
+            if (ParticipantManager.instance.participants.Count() == 0)
+            {
+                Console.WriteLine("this was called at to iniliaze participants");
+                ParticipantManager.instance.GetParticipants();
+            }
+
+
+            questions = new ObservableCollection<Question>();
+            InitializeComponent();
+            questions_list.ItemsSource = questions;
+            participant_list.ItemsSource = ParticipantManager.instance.participants;
+
+        }
+
+        private void TextBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox.Text.Contains("Write your thoughts here"))
+                txtBox.Text = string.Empty;
+        }
+
+        private void participant_list_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var student = (Participant)e.AddedItems[0];
+
+            id_lastSelected = ParticipantManager.instance.participants.IndexOf(student);
+            NoteTextBox.Text = student.Notes;
+        }
+
+        private void NoteTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //change the participants here to  update notes for each student
+            if ((ParticipantManager.instance.participants.Count != 0) && (id_lastSelected < ParticipantManager.instance.participants.Count))
+            {
+                ParticipantManager.instance.participants[id_lastSelected].Notes = NoteTextBox.Text;
+
+            }
+
+        }
+
+        private void Add_Question_Click(object sender, RoutedEventArgs e)
+        {
+            var addQuestionWindow = new AddQuestionWindow();
+            addQuestionWindow.ShowDialog();
+
+        }
+
+
+
+        private void usequestion_Click(object sender, RoutedEventArgs e)
+        {
+            Question requestingTile = (sender as Button).DataContext as Question;
+
+            Console.WriteLine(requestingTile.question);
+
+        }
+
+
+        private void SetupUpGroups_click(object sender, RoutedEventArgs e)
+        {
+
+            var bo_Settings_Window = new BO_Settings_Window();
+            bo_Settings_Window.ShowDialog();
+        }
+    }
+}
---
+++ zoom_sdk_demo/Models/Group.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoom_sdk_demo.Models
+{
+    public class Group
+    {
+        public String Name { get; set; } = "";
+        public ObservableCollection<Participant> Participants_in_group { get; set; } = new ObservableCollection<Participant>();
+
+    }
+
+    public sealed class GroupManager
+    {
+        public static GroupManager instance = new GroupManager();
+        private GroupManager() { }
+        public ObservableCollection<Group> groups = new ObservableCollection<Group>();
+        public int groupSize { get; set; } = 3;
+
+        public void getGroups()
+        {
+            //We need to get groups from particpant manager
+            if (ParticipantManager.instance.participants.Count < 1)
+            {
+                ParticipantManager.instance.GetParticipants();
+            }
+            groups.Clear();
+            Group group = new Group();
+            int count = 0;
+            
+            int groupCount = 1;
+            foreach (var participant in ParticipantManager.instance.participants)
+            {
+                group.Participants_in_group.Add(participant);
+                count++;
+                if (count % groupSize == 0)
+                {
+                    group.Name = "Group " + groupCount.ToString();
+                    groups.Add(group);
+                    groupCount++;
+                    group = new Group();
+                    continue;
+                }
+                //we neeed to make sure we we are in the last count. the rest will hold the remainder of the group.
+                //Continue for the if before holds if we have exactly number of participants divisble by group size
+                if (count == ParticipantManager.instance.participants.Count)
+                {
+                    group.Name = "Group " + groupCount.ToString();
+                    groups.Add(group);
+
+                }
+            }
+        }
+    }
+
+
+}
