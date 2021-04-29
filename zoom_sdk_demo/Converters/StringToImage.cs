8c4133cc1428164c52b09bfd406e17b51b26a90e
Ebrahim Karam <ebrahim_karam@hotmail.com>
2021-04-29 11:25:44 -0400

    Fixes Image Source issue
    now empty image sources shouldn't have a problem on the system

---
+++ zoom_sdk_demo/Converters/StringToImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace zoom_sdk_demo.Converters
+{
+   public class StringToImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString() == "") {
+                return new BitmapImage();
+            }
+            //return new Bitmap();
+            return new BitmapImage(new Uri(value.ToString()));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
